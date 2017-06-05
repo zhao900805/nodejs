49b90fd357b64d7f01e93a4dbb6304513455fcd5
zhao900805 <zhao900805@gmail.com>
2017-06-05 09:52:13 -0700

    Add files via upload

---
+++ public/javascripts/clickActions.js
@@ -0,0 +1,16 @@
+function login(){
+    window.location.href = "/user/loginPage";
+}
+
+function register(){
+    window.location.href = "/user/registerPage";
+}
+/*function Post(){
+    window.location.href = "/homePageView/:name/Addpost";
+}
+function all_post(){
+    window.location.href = "/homePageView/:name/postList";
+}*/
+function toUserPage(){
+    window.location.href = "/user";
+}
